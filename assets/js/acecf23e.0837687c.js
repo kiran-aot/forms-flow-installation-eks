dcf40a4522e8d44bb91f1bb6085f8830ba2d5262
Kiran K A <kiran.ka@aot-technologies.com>
2024-10-15 03:36:57 -0700

    build file udpate

---
+++ assets/js/acecf23e.0837687c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkchart_doc=self.webpackChunkchart_doc||[]).push([[903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
